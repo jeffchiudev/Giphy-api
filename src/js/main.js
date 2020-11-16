db859e1b3a8ed0b51ee11cfae0306ee63028c5b9
Sarah Gilbert <sarahgilbert064@gmail.com>
2020-11-16 15:44:43 -0800

    create multiple btns using giphy api
    co-authored-by: Jeff Chiu <jwchiu82@gmail.com>

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,88 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './../css/styles.css';
+
+$(document).ready(function() {
+  $('#giphySearch').click(function() {
+    const gif = $('#giphy').val();
+    $('#giphy').val("");
+
+    let request = new XMLHttpRequest();
+    const url = `http://api.giphy.com/v1/gifs/search?q=${gif}&api_key=${process.env.API_KEY}&limit=5`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
+
+    function getElements(response) {
+      $('.showGif').append(`<img src="${response.data[0].images.original.url}">`);
+    }
+  });
+
+  $('#giphyTrending').click(function() {
+    let request = new XMLHttpRequest();
+    const url = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}&limit=5&rating=g`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
+
+    function getElements(response) {
+      $('.showGif').append(`<img src="${response.data[0].images.original.url}">`);
+    }
+  });
+  
+  $('#giphyRandom').click(function() {
+    
+    let request = new XMLHttpRequest();
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}&tag=&rating=g`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
+
+    function getElements(response) {
+      $('.showGif').append(`<img src="${response.data.images.original.url}">`);
+    }
+  });
+
+  $('#giphyTranslate').click(function() {
+    const gif = $('#translateGiphy').val();
+    $('#translateGiphy').val("");
+
+    let request = new XMLHttpRequest();
+    const url = `https://api.giphy.com/v1/gifs/translate?api_key=${process.env.API_KEY}&s=${gif}&int=10`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
+
+    function getElements(response) {
+      $('.showGif').append(`<img src="${response.data.images.original.url}">`);
+    }
+  });
+});
